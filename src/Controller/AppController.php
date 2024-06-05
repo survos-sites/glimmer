adc569a17a5ebac2b8f6aa4b04fe0aa32dbcecb9
Tac Tacelosky <tacman@gmail.com>
2024-06-05 15:22:27 -0400

    add bunny

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -2,10 +2,20 @@
 
 namespace App\Controller;
 
+//use Bunny\Storage\Client;
+//use Bunny\Storage\Region;
+use Http\Discovery\Psr18ClientDiscovery;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Survos\FlickrBundle\Services\FlickrService;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use ToshY\BunnyNet\BaseAPI;
+use ToshY\BunnyNet\Client\BunnyClient;
+use ToshY\BunnyNet\EdgeStorageAPI;
+use ToshY\BunnyNet\Enum\Region;
 
 class AppController extends AbstractController
 {
@@ -29,8 +39,55 @@ class AppController extends AbstractController
     }
 
     #[Route('/', name: 'app_homepage')]
-    public function albums(FlickrService $flickr): Response
+    public function albums(FlickrService $flickr,
+    #[Autowire('%env(BUNNY_API_KEY)%')] string $apiKey,
+    HttpClientInterface $httpClient,
+    ): Response
     {
+
+
+//        $httpClient =  Psr18ClientDiscovery::find();
+        $httpClient = new \Symfony\Component\HttpClient\Psr18Client();
+// Create a BunnyClient using any HTTP client implementing "Psr\Http\Client\ClientInterface".
+        $bunnyClient = new BunnyClient(
+            client: $httpClient
+        );
+
+// Provide the API key available at the "Account Settings > API" section.
+        $baseApi = new BaseAPI(
+            apiKey: $apiKey,
+            client: $bunnyClient,
+        );
+//        dd($baseApi->listCountries());
+        $storageZoneName = 'museado';
+        foreach ($baseApi->listStorageZones()->getContents() as $zone) {
+            $accessKey = $zone['ReadOnlyPassword'];
+
+// Provide the "(Read-Only) Password" available at the "FTP & API Access" section of your specific storage zone.
+            $edgeStorageApi = new EdgeStorageAPI(
+                apiKey: $accessKey,
+                client: $bunnyClient,
+                region: Region::NY
+            );
+            $list = $edgeStorageApi->listFiles(
+                storageZoneName: $storageZoneName,
+                path: '/'
+            );
+
+//            $client = new Client($accessKey, 'museado', Region::NEW_YORK);
+//            $list = $client->listFiles('/');
+            foreach ($list->getContents() as $fileInfo) {
+                $subList = $edgeStorageApi->listFiles(
+                    $storageZoneName,
+                    path: $fileInfo['ObjectName']
+                );
+                dd($subList);
+//                dd($client->getContents('/'));
+
+            }
+        };
+        dd();
+
 
         $userId = '26016159@N00';
         $result = $flickr->photosets()->getList($userId);
@@ -39,5 +96,19 @@ class AppController extends AbstractController
             'albums' => $result['photoset']
         ]);
     }
+
+    #[Route('/flickr', name: 'flickr_homepage')]
+    public function flickr(FlickrService $flickr): Response
+    {
+
+
+        $userId = '26016159@N00';
+        $result = $flickr->photosets()->getList($userId);
+
+        return $this->render('albums.html.twig', [
+            'albums' => $result['photoset']
+        ]);
+    }
+
 
 }
